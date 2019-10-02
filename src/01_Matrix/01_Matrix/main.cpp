816d36993b805888d82db2a93d81721e912c7a21
taktaev-artyom <43129320+taktaev-artyom@users.noreply.github.com>
2019-10-02 13:56:14 +0300

    1

--- src/01_Matrix/01_Matrix/main.cpp
+++ src/01_Matrix/01_Matrix/main.cpp
@@ -2,40 +2,12 @@
 
 int main()
 {
-	TVector<double> Vector1(3), Vector2(2), VectorResult(3);
-	TMatrix<double> Matrix1(3), Matrix2(3), Matrix3(2), Matrix5(4), MatrixResult(3);
-	TVector<TVector<double>> VectorOfVectors1(4), VectorOfVectors2(2);
-	for (int i = 0; i < 4; i++)
-	{
-		TVector<double> temp(4 - i);
-		for (int j = i; j < 4; j++)
-		{
-			temp[j - i] = (double)(i + j + 1);
-		}
-		VectorOfVectors1[i] = temp;
-	}
-	std::cout << "Matrix5 = VectorOfVectors1(4)" << std::endl;
+	TVector<double> Vector1(3), Vector2(3), VectorERR(2), VectorRes(3);
+	TMatrix<double> Matrix1(3), Matrix2(3), MatrixERR(2), MatrixRes(3), MatrixVectorofVectors(4);
+	std::cout << "Trying to make Vector3 with -1 size" << std::endl;
 	try
 	{
-		Matrix5 = VectorOfVectors1;
-	}
-	catch (const char* k)
-	{
-		std::cout << k << std::endl;
-	}
-	std::cout << "Matrix5 = VectorOfVectors2(2) ";
-	try
-	{
-		Matrix5 = VectorOfVectors2;
-	}
-	catch (const char* k)
-	{
-		std::cout << k << std::endl;
-	}
-	std::cout << "Matrix4(-3)";
-	try
-	{
-		TMatrix<double> Matrix4(-3);
+		TVector<double> Vector3(-1);
 	}
 	catch (const char* k)
 	{
@@ -43,91 +15,210 @@ int main()
 	}
 	Vector1.random_vector();
 	std::cout << "Vector1" << std::endl << Vector1 << std::endl;
+	Vector2.random_vector();
+	std::cout << "Vector2" << std::endl << Vector2 << std::endl;
+	VectorERR.random_vector();
+	std::cout << "VectorERR" << std::endl << VectorERR << std::endl;
+	std::cout << "Trying to VectorRes = Vector1" << std::endl;
+	try
+	{
+		VectorRes = Vector1;
+		std::cout << "Success" << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Trying to VectorRes = VectorRes" << std::endl;
+	try
+	{
+		VectorRes = VectorRes;
+		std::cout << "Success" << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 == Vector2?" << std::endl;
+	try
+	{
+		if (Vector1 == Vector2) std::cout << "True" << std::endl;
+		if (Vector1 != Vector2) std::cout << "False" << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 + 10:" << std::endl;
+	std::cout << Vector1 + 10 << std::endl;
+	std::cout << "Vector2 - 10:" << std::endl;
+	std::cout << Vector2 - 10 << std::endl;
+	std::cout << "VectorERR * 10:" << std::endl;
+	std::cout << VectorERR * 10 << std::endl;
+	std::cout << "Vector1 + Vector2:" << std::endl;
+	try
+	{
+		VectorRes = Vector1 + Vector2;
+		std::cout << VectorRes << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 + VectorERR:" << std::endl;
+	try
+	{
+		VectorRes = Vector1 + VectorERR;
+		std::cout << VectorRes << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 - Vector2:" << std::endl;
+	try
+	{
+		VectorRes = Vector1 - Vector2;
+		std::cout << VectorRes << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 - VectorERR:" << std::endl;
+	try
+	{
+		VectorRes = Vector1 - VectorERR;
+		std::cout << VectorRes << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 * Vector2:" << std::endl;
+	try
+	{
+		double tmp = Vector1 * Vector2;
+		std::cout << tmp << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1 * VectorERR:" << std::endl;
+	try
+	{
+		double tmp = Vector1 * VectorERR;
+		std::cout << tmp << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1[0] = ";
+	try
+	{
+		std::cout << Vector1[0] << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Vector1[100] = ";
+	try
+	{
+		std::cout << Vector1[100] << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << "Trying to make Matrix3 with -1 size" << std::endl;
+	try
+	{
+		TMatrix<double> Matrix3(-1);
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
+	TVector<TVector<double>> VectorOfVectors(4);
+	for (int i = 0; i < 4; i++)
+	{
+		TVector<double> temp(4 - i);
+		std::cout << "Enter " << i << " row of vector of vectors: " << std::endl;
+		std::cin >> temp;
+		VectorOfVectors[i] = temp;
+	}
+	std::cout << "Trying to MatrixVectorofVectors = VectorOfVectors(4):" << std::endl;
+	try
+	{
+		MatrixVectorofVectors = VectorOfVectors;
+		std::cout << "Success" << std::endl;
+		std::cout << "Matrix5" << std::endl << MatrixVectorofVectors << std::endl;
+	}
+	catch (const char* k)
+	{
+		std::cout << k << std::endl;
+	}
 	Matrix1.random_matrix();
-	Matrix3.random_matrix();
-	std::cin >> Matrix2;
-	std::cout << "MatrixResult = Matrix2:" << std::endl;
-	MatrixResult = Matrix2;
-	std::cout << "Matrix1" << std::endl << Matrix1 << "Matrix2" << std::endl << Matrix2 << "Matrix3" << std::endl << Matrix3;
-	std::cout << " Matrix1 == Matrix2?: ";
+	std::cout << "Matrix1" << std::endl << Matrix1 << std::endl;
+	Matrix2.random_matrix();
+	std::cout << "Matrix2" << std::endl << Matrix2 << std::endl;
+	MatrixERR.random_matrix();
+	std::cout << "MatrixERR" << std::endl << MatrixERR << std::endl;
+	std::cout << "Trying to MatrixRes = Matrix2:" << std::endl;
 	try
 	{
-		if (Matrix1 == Matrix2)
-		{
-			std::cout << "Equal" << std::endl;
-		}
-		else
-		{
-			std::cout << "Different" << std::endl;
-		}
+		MatrixRes = Matrix2;
+		std::cout << "Success" << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << " Matrix1 != Matrix2?: ";
+	std::cout << "Matrix1 == Matrix2?" << std::endl;
 	try
 	{
-		if (Matrix1 != Matrix2)
-		{
-			std::cout << "Different" << std::endl;
-		}
-		else
-		{
-			std::cout << "Equal" << std::endl;
-		}
+		if (Matrix1 == Matrix2) std::cout << "True" << std::endl;
+		if (Matrix1 != Matrix2) std::cout << "False" << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 != Matrix3?: ";
+	std::cout << "Matrix1 == MatrixERR?: ";
 	try
 	{
-		if (Matrix1 != Matrix3)
-		{
-			std::cout << "Different" << std::endl;
-		}
-		else
-		{
-			std::cout << "Equal" << std::endl;
-		}
+		if (Matrix1 == MatrixERR) std::cout << "True" << std::endl;
+		if (Matrix1 != MatrixERR) std::cout << "False" << std::endl;
+		
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 == Matrix3?: ";
-	try
-	{
-		if (Matrix1 == Matrix3)
-		{
-			std::cout << "Equal" << std::endl;
-		}
-		else
-		{
-			std::cout << "Different" << std::endl;
-		}
-	}
-	catch (const char* k)
-	{
-		std::cout << k << std::endl;
-	}
+	std::cout << "Matrix1 + 10:" << std::endl;
+	std::cout << Matrix1 + 10 << std::endl;
+	std::cout << "Matrix2 - 10:" << std::endl;
+	std::cout << Matrix2 - 10 << std::endl;
+	std::cout << "MatrixERR * 10:" << std::endl;
+	std::cout << MatrixERR * 10 << std::endl;
 	std::cout << "Matrix1 + Matrix2:" << std::endl;
 	try
 	{
-		MatrixResult = Matrix1 + Matrix2;
-		std::cout << MatrixResult << std::endl;
+		MatrixRes = Matrix1 + Matrix2;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
-		std::cout << k;
+		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 + Matrix3:";
+	std::cout << "Matrix1 + MatrixERR:";
 	try
 	{
-		MatrixResult = Matrix1 + Matrix3;
-		std::cout << MatrixResult << std::endl;
+		MatrixRes = Matrix1 + MatrixERR;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
@@ -136,18 +227,18 @@ int main()
 	std::cout << "Matrix1 - Matrix2: " << std::endl;
 	try
 	{
-		MatrixResult = Matrix1 - Matrix2;
-		std::cout << MatrixResult;
+		MatrixRes = Matrix1 - Matrix2;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 - Matrix3: ";
+	std::cout << "Matrix1 - MatrixERR: ";
 	try
 	{
-		MatrixResult = Matrix1 - Matrix3;
-		std::cout << MatrixResult;
+		MatrixRes = Matrix1 - MatrixERR;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
@@ -156,44 +247,38 @@ int main()
 	std::cout << "Matrix1 * Matrix2: " << std::endl;
 	try
 	{
-		MatrixResult = Matrix1 * Matrix2;
-		std::cout << MatrixResult;
+		MatrixRes = Matrix1 * Matrix2;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 * Matrix3: ";
+	std::cout << "Matrix1 * MatrixERR: ";
 	try
 	{
-		MatrixResult = Matrix1 * Matrix3;
-		std::cout << MatrixResult;
+		MatrixRes = Matrix1 * MatrixERR;
+		std::cout << MatrixRes << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << "Matrix1 + 1421:" << std::endl;
-	std::cout << Matrix1 + 1412;
-	std::cout << "Matrix2 - 67545:" << std::endl;
-	std::cout << Matrix2 - 67545;
-	std::cout << "Matrix3 * 1564:" << std::endl;
-	std::cout << Matrix3 * 1564;
-	std::cout << "Matrix1 * Vector1:" << std::endl;
+	std::cout << "Matrix1 * Vector1: ";
 	try
 	{
-		VectorResult = Matrix1 * Vector1;
-		std::cout << VectorResult << std::endl;
+		VectorRes = Matrix1 * Vector1;
+		std::cout << VectorRes << std::endl;
 	}
 	catch (const char* k)
 	{
 		std::cout << k << std::endl;
 	}
-	std::cout << " Matrix1 * Vector2: ";
+	std::cout << " Matrix1 * VectorERR: ";
 	try
 	{
-		VectorResult = Matrix1 * Vector2;
-		std::cout << VectorResult << std::endl;
+		VectorRes = Matrix1 * VectorERR;
+		std::cout << VectorRes << std::endl;
 	}
 	catch (const char* k)
 	{
